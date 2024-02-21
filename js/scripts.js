60b2ce37497bb3b5b00776387ed80c694ae44ff1
Unreachedx <oliphant.p@web.de>
2024-02-21 16:15:09 +0100

    Changed the Title, Updated the JS file with Pokemon Array Data

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-alert('Hello world');
-
-const favoritFood = 'Sushi';
-
-document.write(favoritFood);
+let pokemonList = [
+  {
+    name: 'Charizard',
+    height: 1.7,
+    type: ['Fire', 'Flying']
+  },
+  {  
+  name: 'Blastoise',
+  height: 1.6,
+  type: ['Water']
+  },
+  {
+    name: 'Arcanine',
+    height: 1.9,
+    type: ['Fire'] 
+  }
+];
+console.log(pokemonList)
