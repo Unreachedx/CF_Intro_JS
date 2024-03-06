1006a80436067b80be124f7923e34a3b7cfd47a3
Unreachedx <oliphant.p@web.de>
2024-03-06 16:24:13 +0100

    Fixed and added some comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 let pokemonRepository = (function () {
 
-  let pokemonList = [   {
+  let pokemonList = [
+    // Array of pokemon for the Page
+    {
     name: 'Charizard',
     height: 1.7,
     type: ['Fire', 'Flying']
@@ -22,8 +24,14 @@ let pokemonRepository = (function () {
   }]
 
   function add(pokemon) {
-    pokemonList.push(pokemon);
-  };
+    // Check if the parameter is an object before adding it to the list
+    if (typeof pokemon === 'object' && pokemon !== null) {
+      pokemonList.push(pokemon);
+    } else {
+      console.error('Invalid input. Only objects can be added to the pokemonList.');
+    }
+  }
+
   function getAll() {
     return pokemonList;
   }
@@ -46,6 +54,7 @@ pokemonRepository.add(newPokemon);
 pokemonRepository.getAll().forEach((pokemon) => {
   let output = '<div class="pokemonName">' + pokemon.name + "  " + pokemon.height + "  " + pokemon.type;
 
+  // if pokemon.height is bigger then 1.5 they get a extra text to their Name
   if (pokemon.height > 1.5) {
     output += ' - This is really a big Pokemon';
   }
